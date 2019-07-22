2d248337acd2366b35701a1c04fce58b0bcce03c
micahhowe <micahhowe182@gmail.com>
2019-07-22 12:11:30 -0600

    reset the base images

--- server/allMemes.js
+++ server/allMemes.js
@@ -1,6 +1,6 @@
 const data = [
     {
-        img: 'https://i.redd.it/00nyozt7yh021.png',
+        img: 'https://i0.wp.com/www.thesun.co.uk/wp-content/uploads/2017/05/nintchdbpict0003267145422.jpg?crop=58px%2C0px%2C639px%2C426px&resize=1200%2C800&ssl=1',
         id:1
     },
     {
